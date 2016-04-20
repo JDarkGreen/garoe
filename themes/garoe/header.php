ecbfcc56175ec144813766a84ba7c330b792b10c
JDarkGreen <jgomez.4net@gmail.com>
2016-04-20 14:50:31 -0500

    update home

--- themes/garoe/header.php
+++ themes/garoe/header.php
@@ -49,7 +49,10 @@
 					<!-- Telefonos -->
 					<p class="text-phone--green">
 						<?= !empty($options['contact_tel']) ? "Tel.: " . $options['contact_tel'] : '' ?>
-						<?= !empty($options['contact_cel']) ? " Cel.: " . $options['contact_cel'] : '' ?>
+						<!-- Telefono y Celular -->
+						<?= ( !empty($options['contact_tel']) && !empty($options['contact_cel']) ) ? "&nbsp;" : '' ?>
+						<!-- Celular -->
+						<?= !empty($options['contact_cel']) ? "Cel.: " . $options['contact_cel'] : '' ?>
 					</p>
 					<!-- Correo -->
 					<p class="text-email--red">
